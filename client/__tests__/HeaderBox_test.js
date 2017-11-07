187f713c713cb97abe969c4ffbef97ccdbfae0bb
Doug Riblet <smashzen85@gmail.com>
2017-11-07 16:34:52 -0600

    create second test on HeaderBox

--- client/__tests__/HeaderBox_test.js
+++ client/__tests__/HeaderBox_test.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
 import HeaderBox from '../components/HeaderBox'
+import ReactDOM from 'react-dom';
 
 describe('HeaderBox', () => {
   test('renders correctly', () => {
@@ -9,4 +10,12 @@ describe('HeaderBox', () => {
     ).toJSON()
     expect(HeaderBox_tree).toMatchSnapshot()
   })
+
+  test('renders without crashing', () => {
+    const div = document.createElement('div')
+    ReactDOM.render(<HeaderBox />, div)
+  })
 })
+
+
+
