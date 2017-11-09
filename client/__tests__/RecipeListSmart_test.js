73690887ebd1053344d30edf272a76f6c231c31b
Doug Riblet <smashzen85@gmail.com>
2017-11-09 10:08:49 -0600

    create RecipeListSmart_test

---
+++ client/__tests__/RecipeListSmart_test.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import renderer from 'react-test-renderer'
+import { shallow } from 'enzyme'
+import RecipeListSmart from '../components/RecipeListSmart'
+
+
