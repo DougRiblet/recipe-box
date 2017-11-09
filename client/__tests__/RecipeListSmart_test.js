e8340a15a3d51b3b3aab6180d495246b0a23d401
Doug Riblet <smashzen85@gmail.com>
2017-11-09 11:06:59 -0600

    first test for RecipeListSmart

--- client/__tests__/RecipeListSmart_test.js
+++ client/__tests__/RecipeListSmart_test.js
@@ -2,6 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import renderer from 'react-test-renderer'
 import { shallow } from 'enzyme'
+import configureStore from 'redux-mock-store'
 import RecipeListSmart from '../components/RecipeListSmart'
+import initialState from '../initialState/initialState'
 
+describe('RecipeListSmart', () => {
+  const mockStore = configureStore()
+  let store, container
 
+  beforeEach(()=>{
+    store = mockStore(initialState)
+    container = shallow(<RecipeListSmart store={store} />)
+  })
+
+  test('render the smart component', () => {
+    expect(container.length).toEqual(1)
+  })
+})
