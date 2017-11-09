e36b8d438f967ba229e7a65951a88051b610a607
Doug Riblet <smashzen85@gmail.com>
2017-11-09 09:56:40 -0600

    add enzyme render test to RecipeListDumb

--- client/__tests__/RecipeListDumb_test.js
+++ client/__tests__/RecipeListDumb_test.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import renderer from 'react-test-renderer'
+import { shallow } from 'enzyme'
 import RecipeListDumb from '../components/RecipeListDumb'
 
 describe('RecipeListDumb', () => {
@@ -19,8 +20,17 @@ describe('RecipeListDumb', () => {
     const div = document.createElement('div')
     let dummyRecipeTitles = [{id: '101010', title: 'Grilled Cheese Sandwich'}]
     ReactDOM.render(<RecipeListDumb
-        recipeTitles={dummyRecipeTitles}
-        changeActiveRecipe={() => changeActiveRecipe('010101')}
-      />, div)
+      recipeTitles={dummyRecipeTitles}
+      changeActiveRecipe={() => changeActiveRecipe('010101')}
+    />, div)
+  })
+
+  test('renders component', () => {
+    let dummyRecipeTitles = [{id: '101010', title: 'Grilled Cheese Sandwich'}]
+    let wrapper = shallow(<RecipeListDumb
+      recipeTitles={dummyRecipeTitles}
+      changeActiveRecipe={() => changeActiveRecipe('010101')}
+    />)
+    expect(wrapper.length).toEqual(1)
   })
 })
