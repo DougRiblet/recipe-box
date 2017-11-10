bcccb643712a1f46e8e45a63bf6572daea150c3f
Doug Riblet <smashzen85@gmail.com>
2017-11-10 06:21:36 -0600

    snapshot test for MainPaneDumb

---
+++ client/__tests__/MainPaneDumb_test.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import renderer from 'react-test-renderer'
+import MainPaneDumb from '../components/MainPaneDumb'
+import initialState from '../initialState/initialState'
+
+describe('MainPaneDumb', () => {
+  test('Snapshot', () => {
+    const MainPaneDumb_tree = renderer.create(
+      <MainPaneDumb
+        mode={initialState.mode}
+        recipe={initialState.recipes[0]}
+        deleteAllowed={true}
+        updateRecipe={() => updateRecipe()}
+        setMode={() => setMode()}
+        deleteRecipe={() => deleteRecipe()}
+      />
+    ).toJSON()
+    expect(MainPaneDumb_tree).toMatchSnapshot()
+  })
+
+})
