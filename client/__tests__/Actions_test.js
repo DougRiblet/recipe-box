d653945c668b22d4a3a18f30de6808d91769ebf7
Doug Riblet <smashzen85@gmail.com>
2017-11-11 23:21:57 -0600

    third and fourth action tests

--- client/__tests__/Actions_test.js
+++ client/__tests__/Actions_test.js
@@ -1,6 +1,7 @@
 
 import * as actions from '../actions/actions'
 import * as types from '../actions/actionTypes'
+import initialState from '../initialState/initialState'
 
 describe('Action Creators', () => {
   test('call action to change active recipe', () => {
@@ -19,6 +20,26 @@ describe('Action Creators', () => {
       mode: newMode
     }
     expect(actions.setMode(newMode)).toEqual(expectedAction)
+  })
+
+  test('call action to update recipe after editing', () => {
+    const dummyRecipe = initialState.recipes[0]
+    const expectedAction = {
+      type: types.UPDATE_RECIPE,
+      recipe: dummyRecipe,
+      mode: 'display',
+      active: dummyRecipe.id
+    }
+    expect(actions.updateRecipe(dummyRecipe)).toEqual(expectedAction)
+  })
+
+  test('call action to delete recipe', () => {
+    const idString = '345789'
+    const expectedAction = {
+      type: types.DELETE_RECIPE,
+      id: idString
+    }
+    expect(actions.deleteRecipe(idString)).toEqual(expectedAction)
   })
 
 })
