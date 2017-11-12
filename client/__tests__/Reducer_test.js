79f96716610a40e915658261856014098e074ae8
Doug Riblet <smashzen85@gmail.com>
2017-11-12 14:09:59 -0600

    add export statements to actionTypes, first reducer test passing

--- client/__tests__/Reducer_test.js
+++ client/__tests__/Reducer_test.js
@@ -1,3 +1,10 @@
-import * as actions from '../actions/actions'
 import * as types from '../actions/actionTypes'
 import reducer from '../reducers/reducer'
+import initialState from '../initialState/initialState'
+
+describe('Action Creators', () => {
+  test('return state unchanged if no action', () => {
+    expect(reducer(initialState, {})).toEqual(initialState)
+  })
+
+})
