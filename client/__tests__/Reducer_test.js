01c37ffb70a1606db67aba791a77550f6343d6f8
Doug Riblet <smashzen85@gmail.com>
2017-11-12 14:24:21 -0600

    reducer test for setMode passing

--- client/__tests__/Reducer_test.js
+++ client/__tests__/Reducer_test.js
@@ -1,10 +1,28 @@
 import * as types from '../actions/actionTypes'
+import * as actions from '../actions/actions'
 import reducer from '../reducers/reducer'
 import initialState from '../initialState/initialState'
 
 describe('Action Creators', () => {
   test('return state unchanged if no action', () => {
     expect(reducer(initialState, {})).toEqual(initialState)
+  })
+
+  test('process SET_MODE action correctly', () => {
+    let actionSetModeToEdit = actions.setMode('edit')
+    expect(reducer(initialState, actionSetModeToEdit)).toEqual({
+      active: '101010',
+      mode: 'edit',
+      recipes: [
+        {
+          id: '101010',
+          title: 'Grilled Cheese Sandwich',
+          ingredients: '2 slices bread\n\n1 slice cheese\n\n2 dollops butter',
+          directions: 'Spread butter on bread\n\nPlace cheese between bread slices\n\nGrill in pan until bread is lightly browned on both sides',
+          notes: 'Yum yum!'
+        }
+      ]
+    })
   })
 
 })
